83a1caf28528b8ccc5246870fd756666d0fb4817
MaciejBodnar <maciej.bodnar2@gmail.com>
2024-03-27 12:58:14 +0100

    adding files, styling, adding basic js and render function with clear

---
+++ main.js
@@ -0,0 +1,34 @@
+console.log("SimpleTwitter");
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+  databaseURL: "https://mobileapp-shoppingbasket-default-rtdb.europe-west1.firebasedatabase.app/",
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const twittsListInDB = ref(database, "twittsList");
+
+const inputEl = document.getElementById("input-el");
+const btnEl = document.getElementById("btn-el");
+const twitList = document.getElementById("twit-list");
+
+btnEl.addEventListener("click", function () {
+  let inputElValue = inputEl.value;
+
+  render(inputElValue);
+  console.log(inputElValue);
+  clear();
+});
+
+function render(objectToRender) {
+  const newEl = document.createElement("p");
+  newEl.textContent = objectToRender;
+  twitList.append(newEl);
+}
+
+function clear() {
+  inputEl.value = "";
+}
